356196b913150f52baaf2c396035236fb4cbb80f
AlejandroMBJS <a.mejiabjs@gmail.com>
2023-12-07 21:09:30 -0600

    React router- Hook and Register page conf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import RegisterPage from './pages/RegisterPage'
+import LoginPage from './pages/LoginPage'
+
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hola mundo</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/tasks" element={<h1>Tasks Page</h1>} />
+        <Route path="/add-task" element={<h1>new Task</h1>} />
+        <Route path="/tasks/:id" element={<h1>Update Task</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
