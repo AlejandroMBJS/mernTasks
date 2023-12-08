edf83e9ea6066f92fd19b7845491bd1827e2139f
AlejandroMBJS <a.mejiabjs@gmail.com>
2023-12-07 21:22:12 -0600

    cors and backend connect

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,31 +1,36 @@
 import { useForm } from 'react-hook-form'
+import { registerRequest } from '../api/auth'
 
 
 function RegisterPage() {
 
     const { register, handleSubmit } = useForm()
+    const onSubmit = handleSubmit(async values => {
+        console.log(values)
+        const res = await registerRequest(values)
+        console.log(res)
+    })
+
     return (
         <div className='bg-zinc-800 max-w-md p-10 rounded-md'>
 
-            <form onSubmit={handleSubmit(values => {
-                console.log(values)
-            })}>
+            <form onSubmit={onSubmit}>
                 <input type="text"
                     {...register('username', { required: true })}
-                    className='w-full bg-zinc-700 text-gray-50 px-4 py-2 rounded-md my-2' 
+                    className='w-full bg-zinc-700 text-gray-50 px-4 py-2 rounded-md my-2'
                     placeholder='username'
-                    />
+                />
                 <input type="email"
                     {...register('email', { required: true })}
                     className='w-full bg-zinc-700 text-gray-50 px-4 py-2 rounded-md my-2'
                     placeholder='email'
-                    />
+                />
 
                 <input type="password"
-                    {...register('password', { required: true })} 
-                    className='w-full bg-zinc-700 text-gray-50 px-4 py-2 rounded-md my-2' 
+                    {...register('password', { required: true })}
+                    className='w-full bg-zinc-700 text-gray-50 px-4 py-2 rounded-md my-2'
                     placeholder='password'
-                    />
+                />
 
                 <button type='submit'>
                     Register
